9864795eb4346e32083f67b82059be89c0f19011
Daniel <Daniel_PeAya91@hotmail.com>
2019-05-23 00:58:36 -0500

    nuevos archivos

---
+++ application/views/content/team/elopez.php
@@ -0,0 +1,44 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed.');
+
+$img_path = base_url('assets/images').'/';
+
+?>
+
+
+<div class="container" style="margin-top: 120px;">
+	<div class="row">
+
+		<div class="col s12 m4">
+			<div class="card">
+				<div class="card-image">
+					<img src="<?php echo $img_path; ?>team/elopez.png">
+				</div>
+				<div class="card-content">
+					<span class="card-title">Dra. Eliza López Loeza</span>
+					<p>Directora Ejecutiva del CIDAM.</p>
+					<p>Tel: (443) 299-0181 y (443) 299-0264 Ext. 554</p>
+					<p>E-mail: elopez@cidam.org</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="col s12 m8">
+			<h1>Reseña</h1>
+			<p>De formación profesional en la Universidad Michoacana de San Nicolás de Hidalgo, en la Licenciatura de Químico Farmacobiología con enfoque a las áreas de calidad de los diferentes sectores públicos y privados, experiencia desde el año 2007 en acreditación de laboratorios de pruebas o ensayos en ramas como agua, alimentos, química, ciencias, laboratorios de calibración, entre otros, así mismo catedrática de los laboratorios de la Licenciatura en Ingeniería en Biotecnología de la Universidad Michoacana de San Nicolás de Hidalgo y coordinadora del diplomado Acreditación de Laboratorios de Ensayo o Calibración que se imparte en el Laboratorio de Biofísica de la Universidad Michoacana a partir del año 2009 y hasta la fecha.</p>
+			<p>Con distinciones:<br>
+			•	Presidente del Consejo Interno de certificación profesional (CICER), para la certificación de miembros del Colegio de Químicos del Estado de Michoacán, 2018-2020.
+			•	Certificación en el área de Química Forense por parte del Consejo Interno de certificación profesional, 2019-2021.<br>
+			•	Director y Representante autorizada ante la entidad mexicana de acreditación de los laboratorios de calibración en las magnitudes de óptica, masa, volumen, temperatura y presión. No. Acreditación: OP-04, V-54, M-165, T-165 y P-129.<br>
+			•	Director y Gerente Técnico ante la entidad mexicana de acreditación de las Unidades de Verificación en producto mezcal, información comercial NOM-142-SSA1/SCFI-2014 y NOM-051-SCFI/SSA1-2010 e Instrumentos de medición. No. Acreditación: UVNOM-145 y UVNOM-184.<br>
+			•	Comunicador Competente. Toastmasters International. Junio 29 2015.<br>
+			•	Líder competente. Toastmaster International Leadership program. 05 de mayo del 2016.<br>
+			•	Certificación como instructora “Impartición de cursos con enfoque de programación neurolingüística. Aicon Internacional, S.C. 6 de septiembre del 2013.<br>
+			•	Mas de 260 citas a trabajos de investigación en ciencia básica. 2005 al 2019.<br><br>
+			Formación académica:<br>
+			•	2009–2014 Doctorado, Universidad Michoacana de San Nicolás de Hidalgo. Instituto de Física y Matemáticas, Morelia, Michoacán, Doctorado en el área de la Física.<br>
+			•	2002–2005 Maestría, Universidad Michoacana de San Nicolás de Hidalgo. Facultad de Ciencias Médicas y Biológicas “Dr. Ignacio Chávez”, Morelia, Michoacán, Maestría en Farmacología Básica.<br>
+			•	1993-1998 Facultad de Químico Farmacobiología, Universidad Michoacana de San Nicolás de Hidalgo. Ciencias Médicas y Biológicas “Dr. Ignacio Chávez”, Morelia, Michoacán, Químico Farmacobiólogo.
+			</p>
+		</div>
+	</div>
+</div>
