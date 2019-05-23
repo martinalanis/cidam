8d67dc09ac1ae1debc1a9f7faedc7ac368950043
Jaime Rodriguez <jaime_rdez794@live.com.mx>
2019-05-23 00:58:04 -0500

    Grid de workgroup

--- application/views/content/workgroup.php
+++ application/views/content/workgroup.php
@@ -6,298 +6,297 @@ $img_path = base_url('assets/images').'/';
 
 
 <div class="container" style="margin-top: 120px;">
-	<div class="row">
 
-		<div class="col s12 m4 l3">
+	<div id="grid-sizer" class="grid">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/elopez.png">
 					<a href="<?=base_url('grupo-de-trabajo/elopez');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dra. Eliza López Loeza</span>
+					<span class="card-title nombre">Dra. Eliza López Loeza</span>
 					<p>Directora Ejecutiva del CIDAM.</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/lserrano.png">
 					<a href="<?=base_url('grupo-de-trabajo/lserrano');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dr. Luis Enrrique Serrano</span>
-						<p>Coordinador de la Unidad de Gestión de la Innovación.</p>
-                        <p>Responsable del Laboratorio de Inocuidad Microbiologica.</p>
+					<span class="card-title nombre">Dr. Luis Enrrique Serrano</span>
+					<p>Coordinador de la Unidad de Gestión de la Innovación.</p>
+					<p>Responsable del Laboratorio de Inocuidad Microbiologica.</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/rperez.png">
 					<a href="<?=base_url('grupo-de-trabajo/rperez');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dra. Rocio Viridiana Perez Gallardo</span>
-                        <p>Responsable del Laboratorio de Inocuidad Quimica.</p>
+					<span class="card-title nombre">Dra. Rocio Viridiana Perez Gallardo</span>
+					<p>Responsable del Laboratorio de Inocuidad Quimica.</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/ccolin.png">
 					<a href="<?=base_url('grupo-de-trabajo/ccolin');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dra. Citlali Colin Chavez</span>
-                        <p>Responsable del Laboratorio de Poscosecha</p>
+					<span class="card-title nombre">Dra. Citlali Colin Chavez</span>
+					<p>Responsable del Laboratorio de Poscosecha</p>
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/csosa.png">
 					<a href="<?=base_url('grupo-de-trabajo/csosa');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dr. Carlos Sosa Aguirre</span>
+					<span class="card-title nombre">Dr. Carlos Sosa Aguirre</span>
 
 					<p>Coordinador de la Unidad de Biotecnología.</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/mmartinez.png">
 					<a href="<?=base_url('grupo-de-trabajo/mmartinez');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dr. Miguel Ángel Martínez Téllez</span>
-                        <p>Investigador Comisionado del CIAD en el CIDAM.</p>
+					<span class="card-title nombre">Dr. Miguel Ángel Martínez Téllez</span>
+					<p>Investigador Comisionado del CIAD en el CIDAM.</p>
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/jaltamirano.png">
 					<a href="<?=base_url('grupo-de-trabajo/jaltamirano');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dr. Josué Altamirano Hernández</span>
+					<span class="card-title nombre">Dr. Josué Altamirano Hernández</span>
 
 					<p>Respoonsable del Laboratorio de Nutrición Vegetal</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/jvirgen.png">
 					<a href="<?=base_url('grupo-de-trabajo/jvirgen');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dr. José Juan Virgen Ortiz</span>
-                        <p>Responsable del Laboratorio de Poscosecha.</p>
+					<span class="card-title nombre">Dr. José Juan Virgen Ortiz</span>
+					<p>Responsable del Laboratorio de Poscosecha.</p>
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/fesquivel.png">
 					<a href="<?=base_url('grupo-de-trabajo/fesquivel');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dra. Fabiola Esquivel Chavez</span>
-                        <p>Responsable del Laboratorio de Fitopatologia.</p>
+					<span class="card-title nombre">Dra. Fabiola Esquivel Chavez</span>
+					<p>Responsable del Laboratorio de Fitopatologia.</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/eloera.png">
 					<a href="<?=base_url('grupo-de-trabajo/eloera');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dra. Esperanza Loera Alvarado</span>
+					<span class="card-title nombre">Dra. Esperanza Loera Alvarado</span>
 					<p>Responsable del Laboratorio de Análisis de Alimentos y Bebidas</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/aluna.png">
 					<a href="<?=base_url('grupo-de-trabajo/aluna');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dr. Alfonso Luna Cruz</span>
+					<span class="card-title nombre">Dr. Alfonso Luna Cruz</span>
 
 					<p>Responsable del Laboratorio de Manejo de Plagas</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/npano.png">
 					<a href="<?=base_url('grupo-de-trabajo/npano');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Dra. Norma Susana Pano Farias</span>
+					<span class="card-title nombre">Dra. Norma Susana Pano Farias</span>
 					<p>Investigador Asociado del Laboratorio de Poscosecha</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/mrubio.png">
 					<a href="<?=base_url('grupo-de-trabajo/mrubio');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. Marysol Rubio Landa</span>
+					<span class="card-title nombre">M.C. Marysol Rubio Landa</span>
 
 					<p>Investigador asociado responsable del Laboratorio 1 “Planta Piloto” de la Unidad de Biotecnología</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/mpanduro.png">
 					<a href="<?=base_url('grupo-de-trabajo/mpanduro');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. María Guadalupe Panduro Rivera</span>
+					<span class="card-title nombre">M.C. María Guadalupe Panduro Rivera</span>
 					<p>Investigador Asociado del Laboratorio de Inocuidad Química</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/lfarias.png">
 					<a href="<?=base_url('grupo-de-trabajo/lfarias');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. Lorena Farías Rosales</span>
+					<span class="card-title nombre">M.C. Lorena Farías Rosales</span>
 					<p>Investigador Asociado del Laboratorio de Biotecnología Microbiana</p>			
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/asosa.png">
 					<a href="<?=base_url('grupo-de-trabajo/asosa');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. Alejandro Sosa Sales</span>
+					<span class="card-title nombre">M.C. Alejandro Sosa Sales</span>
 					<p>Responsable del Laboratorio de AgroCibernética</p>
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/rfulgencio.png">
 					<a href="<?=base_url('grupo-de-trabajo/rfulgencio');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. Rodolfo Fulgencio Negrete</span>
+					<span class="card-title nombre">M.C. Rodolfo Fulgencio Negrete</span>
 
 					<p>Investigador Asociado del Laboratorio de Interacción Planta-Suelo-Microorganismo</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/caguilar.png">
 					<a href="<?=base_url('grupo-de-trabajo/caguilar');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. Cynthia Laura Aguilar Gil</span>
+					<span class="card-title nombre">M.C. Cynthia Laura Aguilar Gil</span>
 
 					<p>Investigador Asociado del Laboratorio de Inocuidad Microbiológica</p>
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/cmelendez.png">
 					<a href="<?=base_url('grupo-de-trabajo/cmelendez');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">M.C. </span>
+					<span class="card-title nombre">M.C. </span>
 
 					<p>Encargado de Vinculación y Proyecto</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/taltamirano.png">
 					<a href="<?=base_url('grupo-de-trabajo/taltamirano');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">L.A. Teofilo Altamirano Vera</span>
+					<span class="card-title nombre">L.A. Teofilo Altamirano Vera</span>
 					<p>Responsable del Área de Administración y Contabilidad</p>
 				</div>
 			</div>
 		</div>
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/mgomez.png">
 					<a href="<?=base_url('grupo-de-trabajo/mgomez');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Ing. Miguel Ángel Gómez Romero</span>
+					<span class="card-title nombre">Ing. Miguel Ángel Gómez Romero</span>
 
 					<p>Investigador Asociado del Laboratorio de AgroCibernética</p>
 				</div>
 			</div>
 		</div>
 
-		<div class="col s12 m4 l3">
+		<div class="grid-item">
 			<div class="card">
 				<div class="card-image">
 					<img src="<?php echo $img_path; ?>team/oguzman.png">
 					<a href="<?=base_url('grupo-de-trabajo/oguzman');?>" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
 				</div>
 				<div class="card-content">
-					<span class="card-title">Ing. Otoniel Guzmán Talavera</span>
+					<span class="card-title nombre">Ing. Otoniel Guzmán Talavera</span>
 
 					<p>Analista del Laboratorio de Inocuidad Microbiológica</p>
 				</div>
 			</div>
 		</div>
-											
-
 	</div>
+
 </div>
--- assets/js/view_controllers/workgroup_vs.js
+++ assets/js/view_controllers/workgroup_vs.js
@@ -3,5 +3,20 @@ Pace.on('done', function(){
 });
 
 function init(){
+	var $grid = $('.grid');
 
+	$grid.imagesLoaded( function(){
+		$grid.isotope({
+			layoutMode: 'fitRows',
+			itemSelector : '.grid-item',
+			getSortData: {
+				nombre: '.nombre'
+			}
+		});
+	});
+
+	$('.sort-button-group').on( 'click', 'button', function() {
+		var sortValue = $(this).attr('data-sort');
+		$grid.isotope({ sortBy: sortValue });
+	});
 }
